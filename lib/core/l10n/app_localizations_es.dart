2e3589102561d3c1026c927f5b56d27a285b2597
Bryan Cubillos <bcubillos@etherealapps.com>
2026-04-22 12:17:55 -0500

    feat: add app splash screen and launcher icon

---
+++ lib/core/l10n/app_localizations_es.dart
@@ -0,0 +1,22 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for Spanish Castilian (`es`).
+class AppLocalizationsEs extends AppLocalizations {
+  AppLocalizationsEs([String locale = 'es']) : super(locale);
+
+  @override
+  String get appTitle => 'The Movie App';
+
+  @override
+  String get errorNetwork => 'Sin conexión a internet';
+
+  @override
+  String get errorUnknown => 'Error desconocido';
+
+  @override
+  String get errorServer => 'Error del servidor';
+}
